2ef62cf71e450cba1005c790d4e2a49e0ca5187f
asd0905 <dydcodydco@daum.net>
2024-05-23 17:44:14 +0900

    Add: UserCard component

---
+++ src/app/(afterLogin)/_component/UserCard.tsx
@@ -0,0 +1,32 @@
+import { Card } from '@/components/ui/card';
+import { faker } from '@faker-js/faker';
+import Link from 'next/link';
+import style from './userCard.module.css';
+import { Button } from '@/components/ui/button';
+import { UserPlus } from 'lucide-react';
+
+export default function UserCard() {
+  const user = {
+    id: faker.string.nanoid(10),
+    nickname: faker.internet.userName(),
+    age: faker.number.int({ min: 20, max: 50 }),
+    distance: faker.number.int({ min: 5, max: 100 }),
+    area: faker.location.city(),
+    image: faker.image.urlLoremFlickr({ category: 'cat' }),
+  }
+  return (
+    <Card className={style.userCard}>
+      <Link href={`/${user.id}`}>
+        <img src={user.image} className='rounded-xl' alt="img" />
+        <div className={style.userInfo}>
+          <h2 className='text-white font-extrabold text-xl'>{user.nickname}, {user.age}</h2>
+          <p className='text-white font-semibold text-base'>{user.distance}km, {user.area}</p>
+          <Button variant={'default'} className='mt-3 bg-white'>
+            <UserPlus color='#000000' />
+            <span className='ml-2 text-black font-extrabold'>친구신청</span>
+          </Button>
+        </div>
+      </Link>
+    </Card>
+  )
+}
--- src/app/(afterLogin)/page.tsx
+++ src/app/(afterLogin)/page.tsx
@@ -1,10 +1,22 @@
 import MbtiCarousel from './(home)/_component/MbtiCarousel';
+import UserCard from './_component/UserCard';
 
 export default function Page() {
   return (
     <div>
       <MbtiCarousel />
-      <div className='flex flex-col gap-2 px-2 pb-[50px]'>
+      <div className='flex flex-col gap-2 px-2 pb-[50px] pt-2' style={{height: '600dvh'}}>
+        <UserCard />
+        <UserCard />
+        <UserCard />
+        <UserCard />
+        <UserCard />
+        <UserCard />
+        <UserCard />
+        <UserCard />
+        <UserCard />
+        <UserCard />
+        <UserCard />
       </div>
     </div>
   )
