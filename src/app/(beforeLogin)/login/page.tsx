45b923b7606421742b6eed9b9148c395279c62fc
asd0905 <dydcodydco@daum.net>
2024-05-26 23:39:43 +0900

    서버 컴포넌트에서 Server Actions 사용하기

--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -1,53 +1,68 @@
-"use client"
-
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
 import { Button } from "@/components/ui/button"
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import style from './login.module.css';
 import Title from '../_component/Title'
-import React, { useCallback } from 'react'
-import { useRouter } from 'next/navigation'
-
-const FormSchema = z.object({
-  id: z.string().min(2, {
-    message: "id must be at least 2 characters.",
-  }),
-  password: z.string().min(2, {
-    message: "id must be at least 2 characters.",
-  })
-})
+import React, { FormEventHandler } from 'react'
+import { redirect } from 'next/navigation';
 
 const Page = () => {
-  const router = useRouter();
-  const form = useForm<z.infer<typeof FormSchema>>({
-    resolver: zodResolver(FormSchema),
-    defaultValues: {
-      id: "",
-      password: '',
-    },
-  })
+  // const router = useRouter();
+  // const form = useForm<z.infer<typeof FormSchema>>({
+  //   resolver: zodResolver(FormSchema),
+  //   defaultValues: {
+  //     id: "",
+  //     password: '',
+  //   },
+  // })
 
-  const onSubmit = useCallback((data: z.infer<typeof FormSchema>) => {
-    console.log(data);
-    router.push('/userSetting');
-  }, [router]);
+  // const onSubmit = useCallback((data: z.infer<typeof FormSchema>) => {
+  //   console.log(data);
+  //   router.push('/userSetting');
+  // }, [router]);
+
+  const submit = async (formData: FormData) => {
+    'use server';
+    console.log(formData.get('id'));
+    
+    if (!formData.get('id')) {
+      return {message: 'no_id'};
+    }
+    if (!formData.get('password')) {
+      return {message: 'no_password'};
+    }
+    let shouldRedirect = false;
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/login`, {
+        method: 'post',
+        body: formData,
+        credentials: 'include', // 이게 있어야 쿠키전달된다.
+      });
+      console.log(response.status);
+      console.log(await response.json());
+      if (response.status === 403) {
+        return { message: 'user_exists' };
+      }
+      shouldRedirect = true;
+    } catch (err) {
+      console.error(err);
+      return;
+    }
+
+    if (shouldRedirect) {
+      redirect('/'); // redirect는 try catch문에 있으면 안된다.
+    }
+  };
 
   return (
     <section className={style.loginSection}>
       <Title />
-      
-      <Form {...form}>
+      <form action={submit}>
+        <Input id='id' name='id' type='text' placeholder="id" />
+        <Input id='password' name='password' type='password' placeholder="password" />
+
+        <Button type="submit">로그인</Button>
+      </form>
+      {/* <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className={style.loginForm}>
           <FormField
             control={form.control}
@@ -84,7 +99,7 @@ const Page = () => {
           />
           <Button type="submit">login</Button>
         </form>
-      </Form>
+      </Form> */}
     </section>
   )
 }
