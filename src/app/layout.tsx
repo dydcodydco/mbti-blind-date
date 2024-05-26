422ad90286d9aa1ff5a479637beca8de1cfbacae
asd0905 <dydcodydco@daum.net>
2024-05-26 22:51:09 +0900

    Feat: next용 msw 컴포넌트와 .env

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Head from 'next/head';
+import { MSWComponent } from './_component/MSWComponent';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,6 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
+        <MSWComponent />
         {children}
       </body>
     </html>
