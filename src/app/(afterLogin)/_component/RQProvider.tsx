46d1a7e665815e040c2257a334295511a25d8549
asd0905 <dydcodydco@daum.net>
2024-05-27 19:10:02 +0900

    Fixed: RQProvieder.tsx 에 'use client' 문구 추가

--- src/app/(afterLogin)/_component/RQProvider.tsx
+++ src/app/(afterLogin)/_component/RQProvider.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactNode, useState } from 'react'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
