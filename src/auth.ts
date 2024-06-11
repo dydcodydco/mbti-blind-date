89efb611160f33f6b44c12e3983c99c3527c2b74
asd0905 <dydcodydco@daum.net>
2024-06-11 21:00:35 +0900

    Feat: 로그인 후 브라우저에 쿠키 심어주기

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth"
 import Credentials from "next-auth/providers/credentials"
+import { cookies } from 'next/headers';
+import cookie from 'cookie';
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
   pages: {
@@ -16,6 +18,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       },
       authorize: async (credentials) => {
         try {
+          // 백엔드서버로 요청보낼 때 토큰을 쿠키에 넣어서 보내주고 있다.
           const authResponse = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/login`, 
             {
               method: 'POST',
@@ -26,7 +29,18 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
             }
           );
           
-          console.log(authResponse.ok, '-----------------------------authResponse.ok');
+          // 프론트서버에서 백엔드서버의 로그인 토큰을 받아온것. 토큰은 문자열이라서
+          // cookie라이브러리로 객체로 만들어준다.
+          let setCookie = authResponse.headers.get('Set-Cookie');
+          console.log('set-cookie', setCookie);
+          if (setCookie) {
+            const parsed = cookie.parse(setCookie);
+            // 브론트서버에서 브라우저에 쿠키를 심어준다.
+            // 프론트서버에 쿠키를 심으면 안된다! 왜냐하면 프론트서버는 서버라서 공용이다.
+            // 여러 브라우저가 전부 프론트서버르 바라본다. 개인정보 유출 문제 발생할 수 있다.
+            cookies().set('connect.sid', parsed['connect.sid'], parsed); // parsed = 나머지 옵션들
+          }
+
           if (!authResponse.ok) {
             return null;
           }
