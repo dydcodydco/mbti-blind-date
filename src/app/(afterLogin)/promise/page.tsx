f5ff34e036b34ad50826e1a77dd2a15b6945ddd8
asd0905 <dydcodydco@daum.net>
2024-06-26 02:56:29 +0900

    Feat: promise 페이지 로딩 수정

--- src/app/(afterLogin)/promise/page.tsx
+++ src/app/(afterLogin)/promise/page.tsx
@@ -2,8 +2,8 @@ import MainTitle from '../_component/MainTitle';
 import { Metadata } from 'next';
 import PromiseFormButton from './_component/PromiseFormButton';
 import { Suspense } from 'react';
-import Loading from '../(home)/loading';
 import PromiseSectionSuspense from './_component/PromiseSectionSuspense';
+import Loading from './loading';
 
 export const metadata: Metadata = {
   title: '약속'
@@ -16,7 +16,7 @@ export default async function PromisePage() {
       <MainTitle>약속</MainTitle>
       <PromiseFormButton />
       <div className='p-2 gap-2 flex flex-col pb-[50px]'>
-      <Suspense fallback={<Loading className='mt-20' />}>
+        <Suspense fallback={<Loading className='mt-20' />}>
           <PromiseSectionSuspense />
         </Suspense>
       </div>
